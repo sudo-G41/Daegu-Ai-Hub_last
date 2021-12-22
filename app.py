f124cfa7cb66144c9aaf58938689a2fdd5f53092
sudo-G41 <usa7372@gmail.com>
2021-12-22 21:16:37 +0900

    임시

--- app.py
+++ app.py
@@ -1,25 +1,51 @@
-from flask import Flask
-import os
+from itertools import count
+from flask import Flask, request, redirect, render_template, jsonify
+import test3
+from threading import Thread
+import time
 
 app = Flask(__name__)
 
+def threaded_task():
+    print("흠...")
+    time.sleep(3)
+    print("end")
+
 @app.route("/")
-def index():
-    f = open("index.html","r")
-    page = f.read()
-    return page
+def main():
+    print("main start")
+    return render_template("main.html")
 
-@app.route("/h")
-def h():
-    f = open("1.html","r")
-    page = f.read()
-    return page
+@app.route("/video")
+def inputUrl():
+    print("input start")
+    return render_template("inputUrl.html")
 
-@app.route("/c")
-def c():
+@app.route("/video_download", methods=["POST"])
+def video_download():
+    url = request.form.get("YoutubeURL")
+    print("video_download start")
+    global status
+    status = Thread(target=threaded_task)
+    status.daemon = False
+    status.start()
+    print("URL = [",url,"]")
+    print("load status :",status.is_alive())
+    return render_template("3.html")
+
+@app.route("/video_downloading", methods=["POST"])
+def video_downloading():
+    global status
+    print("loading status :",status.is_alive())
+    return jsonify({
+        "stauts":str("loading..." if status.is_alive() else " "),
+        "alive":status.is_alive()
+    })
+
+@app.route("/t")
+def video():
     f = open("2.html","r")
     page = f.read()
-    # os.system("yt-dlp -F https://youtu.be/0kIMgvotSgM")
     return page
 
 app.run(debug=True,host="0.0.0.0",port="8000")
