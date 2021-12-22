ffeaf08927e19864fe7ffb5f12714e902270b641
sudo-G41 <usa7372@gmail.com>
2021-12-23 03:13:13 +0900

    서버내 저장 가능 그 외 악업 필요

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 from itertools import count
+from os import stat
+from re import S
 from flask import Flask, request, redirect, render_template, jsonify
-import test3
+import youtube
 from threading import Thread
 import time
 
 app = Flask(__name__)
 
-def threaded_task():
-    print("흠...")
+# def threaded_task():
+#     print("흠...")
+#     time.sleep(3)
+#     print("end")
+def threaded_task(url):
+    print("download start")
+    youtube.download(url)
+    print("download end")
+def threaded_start():
+    print("ttttt")
     time.sleep(3)
-    print("end")
+    print("ttttt")
 
 @app.route("/")
 def main():
     print("main start")
+    global url
+    url=""
+    # global status
+    # status=1
     return render_template("main.html")
 
 @app.route("/video")
@@ -23,29 +37,52 @@ def inputUrl():
 
 @app.route("/video_download", methods=["POST"])
 def video_download():
+    global url
     url = request.form.get("YoutubeURL")
     print("video_download start")
-    global status
-    status = Thread(target=threaded_task)
-    status.daemon = False
-    status.start()
+    # global status
+    # print("?????????")
+    # print("load status :",status.is_alive())
     print("URL = [",url,"]")
-    print("load status :",status.is_alive())
     return render_template("3.html")
 
 @app.route("/video_downloading", methods=["POST"])
-def video_downloading():
+def video_downloading1():
+    global url
+    print("제발",url,type(url))
     global status
-    print("loading status :",status.is_alive())
+    status = Thread(target=threaded_task(url))
+    status.daemon = False
+    status.start()
+    # print("loading status :",status.is_alive())
+    # return jsonify({
+    #     "stauts":str("loading..." if status.is_alive() else " "),
+    #     "alive":status.is_alive()
+    # })
+    # return redirect("/")
+    print("is alive?",status.is_alive())
+    while status.is_alive():
+        time.sleep(1)
+        print("now alive?")
+
     return jsonify({
-        "stauts":str("loading..." if status.is_alive() else " "),
-        "alive":status.is_alive()
+        "alive":"alive",
+        "url":url
     })
 
-@app.route("/t")
-def video():
-    f = open("2.html","r")
-    page = f.read()
-    return page
+@app.route("/video_downloading/l", methods=["POST"])
+def video_downloading():
+    global url
+    return jsonify({
+        "alive":"alive",
+        "url":url
+    })
+
+@app.route("/video_file/<tag>")
+def video_file_download(tag):
+    global url
+    print(tag)
+    #tag로 파일 찾아서 넘겨주기
+    return render_template("loader.html")
 
 app.run(debug=True,host="0.0.0.0",port="8000")
