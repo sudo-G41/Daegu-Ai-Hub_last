cadbd16a6878c797fdc664c9da7038098cdd653d
sudo-G41 <usa7372@gmail.com>
2021-12-26 12:47:54 +0900

    ????

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def video_file_download3(tag):
 def thumbnail():
     return render_template("Thumbnail.html")
 
-app.run(debug=True,host="0.0.0.0",port="8000")
+app.run(debug=True,host="0.0.0.0",port="25565")
